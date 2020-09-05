ca6e52dbabca9555064a58c3e898e45674abafcd
Justin Baum <justin.baum@ninefx.com>
2020-09-05 01:42:31 -0400

    bologna

--- bossFight/solutions/solutionjb2.py
+++ bossFight/solutions/solutionjb2.py
@@ -1,27 +1,28 @@
 player = 100
 monster = int(input())
-bar = 50
-cooldown = 0
+bar = 56
+cooldown = -1
 
 for i in range(monster*8):
-    if player < 0:
+    if player <= 0:
         break
-    if monster < 0:
+    if monster <= 0:
         break
     if i % 3 == 0 and player < bar:
         player = min(100, player+15)
         print("2")
         cooldown = i+2
-    elif i > cooldown and i % 2:
+    elif i > cooldown and i % 2 == 0:
         print("1")
         monster -= 9
     else:
         print("0")
+    if i % 20 == 0 and i > 10:
+        player = (player+1)//2
     if i % 5 == 0 and i > 0:
         player -= 11
-    if i % 14 in [1,2,3] and i > 10:
+    if i % 14 in [1,2,0] and i > 10:
         player -= 6
-    if i % 20 == 0 and i > 10:
-        player = int((player+1)/2)
-    # print("%s %s %s" % (i, player, monster))
+    print("%s %s %s" % (i, player, monster))
+
 
