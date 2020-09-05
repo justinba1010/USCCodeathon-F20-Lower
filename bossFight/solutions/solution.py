926e9620be0ae199628091595b3276039c9b17dd
Goldenlion5648 <coboudinot@gmail.com>
2020-09-05 00:37:38 -0400

    working algo 3

--- bossFight/solutions/solution.py
+++ bossFight/solutions/solution.py
@@ -1,9 +1,10 @@
-# bossHealth = 15
+# bossHealth = 10000
 bossHealth = int(input())
 playerHealth = 100
 timestep = 0
 restCooldown = 0
 poisonCooldown = 0
+shouldHeal = False
 
 while bossHealth > 0 and playerHealth > 0:
     restCooldown = max(restCooldown - 1, 0)
@@ -15,28 +16,42 @@ while bossHealth > 0 and playerHealth > 0:
              break
     if restCooldown == 0:
         #this block is one potential solution
-        if timestep % 3 == 0 and playerHealth < 60 and timestep % 50 != 0: # any number from 58 to 66 works in a decent amount of time
-            restCooldown = 3
-            playerHealth = min(100, playerHealth + 15)
-            print(2)
-        elif timestep % 2 == 0 and restCooldown == 0:
-            bossHealth -= 9
-            print(1)
-        else:
-            print(0)
+        # if timestep % 3 == 0 and playerHealth < 60 and timestep % 50 != 0: # any number from 58 to 66 works in a decent amount of time
+        #     restCooldown = 3
+        #     playerHealth = min(100, playerHealth + 15)
+        #     print(2)
+        # elif timestep % 2 == 0 and restCooldown == 0:
+        #     bossHealth -= 9
+        #     print(1)
+        # else:
+        #     print(0)
+
 
 
         #this is a second solution, seems most optimal, even without playerHealth < 60
-        if timestep % 3 == 0 and timestep % 40 != 0 and playerHealth < 60: #uses 40 because that is when the monster halves the player health (20 is too frequent)
+        # if timestep % 3 == 0 and timestep % 40 != 0 and playerHealth < 60: #uses 40 because that is when the monster halves the player health (20 is too frequent)
+        #     restCooldown = 3
+        #     playerHealth = min(100, playerHealth + 15)
+        #     print(2)
+        # elif timestep % 2 == 0:
+        #     bossHealth -= 9
+        #     print(1)
+        # else:
+        #     print(0)
+
+        #working solution 3
+        if timestep % 6 == 0:
+            shouldHeal = not shouldHeal
+
+        if timestep % 2 == 0 and playerHealth > 40 and not shouldHeal :
+            bossHealth -= 9
+            print(1)
+        elif timestep % 3 == 0: 
             restCooldown = 3
             playerHealth = min(100, playerHealth + 15)
             print(2)
-        elif timestep % 2 == 0:
-            bossHealth -= 9
-            print(1)
         else:
             print(0)
-
 
         
         #naive solution 1, works on low boss health
