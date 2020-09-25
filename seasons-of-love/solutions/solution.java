6073e165387ca51c2efad84e38ef4ad6c902a920
Goldenlion5648 <coboudinot@gmail.com>
2020-09-25 13:23:02 -0400

    added java solution

---
+++ seasons-of-love/solutions/solution.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Solution
+{
+
+    public static void main(String args[])
+    {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        String unit = input.next();
+        if(unit.equals("s"))
+        {
+            System.out.println(num);
+        }
+        else if(unit.equals("m"))
+        {
+            System.out.println(num * 60);
+        }
+        else if(unit.equals("h"))
+        {
+            System.out.println(num * 60 * 60);
+        }
+        else if(unit.equals("d"))
+        {
+            System.out.println(num * 60 * 60 * 24);
+        }
+        else if(unit.equals("w"))
+        {
+            System.out.println(num * 60 * 60 * 24 * 7);
+        }
+        else if(unit.equals("mo"))
+        {
+            System.out.println(num * 60 * 60 * 24 * 7 * 30);
+        }
+        else if(unit.equals("mo"))
+        {
+            System.out.println(num * 60 * 60 * 24 * 7 * 30 * 365);
+        }
+        
+        input.close();
+
+    }
+
+}
