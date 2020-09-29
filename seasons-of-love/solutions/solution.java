5e93e695b37d63fd505ab33619e7fa0bc3d335db
Goldenlion5648 <coboudinot@gmail.com>
2020-09-29 14:43:02 -0400

    fixed java solution

--- seasons-of-love/solutions/solution.java
+++ seasons-of-love/solutions/solution.java
@@ -6,37 +6,43 @@ public class Solution
     public static void main(String args[])
     {
         Scanner input = new Scanner(System.in);
-        int num = input.nextInt();
-        String unit = input.next();
-        if(unit.equals("s"))
+        int cases = input.nextInt();
+        for(int i = 0; i < cases; ++i)
         {
-            System.out.println(num);
+
+            int num = input.nextInt();
+            String unit = input.next();
+            // System.out.println(num + "  " + unit);
+            if(unit.equals("s"))
+            {
+                System.out.println(num);
+            }
+            else if(unit.equals("m"))
+            {
+                System.out.println(num * 60);
+            }
+            else if(unit.equals("h"))
+            {
+                System.out.println(num * 60 * 60);
+            }
+            else if(unit.equals("d"))
+            {
+                System.out.println(num * 60 * 60 * 24);
+            }
+            else if(unit.equals("w"))
+            {
+                System.out.println(num * 60 * 60 * 24 * 7);
+            }
+            else if(unit.equals("mo"))
+            {
+                System.out.println(num * 60 * 60 * 24 * 30);
+            }
+            else if(unit.equals("y"))
+            {
+                System.out.println(num * 60 * 60 * 24 * 365);
+            }
         }
-        else if(unit.equals("m"))
-        {
-            System.out.println(num * 60);
-        }
-        else if(unit.equals("h"))
-        {
-            System.out.println(num * 60 * 60);
-        }
-        else if(unit.equals("d"))
-        {
-            System.out.println(num * 60 * 60 * 24);
-        }
-        else if(unit.equals("w"))
-        {
-            System.out.println(num * 60 * 60 * 24 * 7);
-        }
-        else if(unit.equals("mo"))
-        {
-            System.out.println(num * 60 * 60 * 24 * 7 * 30);
-        }
-        else if(unit.equals("mo"))
-        {
-            System.out.println(num * 60 * 60 * 24 * 7 * 30 * 365);
-        }
-        
+
         input.close();
 
     }
